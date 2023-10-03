6910718c2025b7fee9726c01787ea22e35337598
Uldis <uj@mobilly.lv>
2023-10-03 13:32:10 +0300

    feat: Set default Http handling address to localhost port 80

--- urlcron.go
+++ urlcron.go
@@ -13,10 +13,12 @@ import (
 func main() {
 	go func() {
 		http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-			_, _ = fmt.Fprint(writer, metric.PrometheusDump())
-
+			_, _ = fmt.Fprint(writer, "<a href=/metrics>metrics</a>")
 		})
-		log.Fatal(http.ListenAndServe(env("ADDR", ":8080"), nil))
+		http.HandleFunc("/metrics", func(writer http.ResponseWriter, request *http.Request) {
+			_, _ = fmt.Fprint(writer, metric.PrometheusDump())
+		})
+		log.Fatal(http.ListenAndServe(env("ADDR", ":80"), nil))
 	}()
 
 	loaders := schedule.LoaderSet{Loaders: []schedule.Loader{
